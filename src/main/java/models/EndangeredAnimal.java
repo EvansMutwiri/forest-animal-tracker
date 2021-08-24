8743ee021c3139256f4ac7cac13c92eef1acd1a5
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-24 08:52:56 +0300

    add user interface

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -11,7 +11,7 @@ public class EndangeredAnimal  extends Animal {
     public static final String YOUNG = "young";
     public static final String NEWBORN = "newborn";
     public static final String HEALTHY = "healthy";
-    public static final String AVERAGE = "average healthy";
+    public static final String OKAY = "okay";
     public static final String ILL = "Ill";
 
     public EndangeredAnimal(String name,String health, String age){
